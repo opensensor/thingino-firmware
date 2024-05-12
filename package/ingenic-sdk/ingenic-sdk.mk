03b769cb2c8a3301f24f19942b743f9d5c3009f6
Matt Davis <matteius@gmail.com>
2024-05-12 09:00:15 -0400

    Check pt

--- package/ingenic-sdk/ingenic-sdk.mk
+++ package/ingenic-sdk/ingenic-sdk.mk
@@ -1,5 +1,5 @@
 INGENIC_SDK_SITE_METHOD = git
-INGENIC_SDK_SITE = https://github.com/themactep/ingenic-sdk
+INGENIC_SDK_SITE = https://github.com/opensensor/ingenic-sdk
 INGENIC_SDK_VERSION = $(shell git ls-remote $(INGENIC_SDK_SITE) master | head -1 | cut -f1)
 
 INGENIC_SDK_LICENSE = GPL-3.0
@@ -7,7 +7,7 @@ INGENIC_SDK_LICENSE_FILES = LICENSE
 
 INGENIC_SDK_MODULE_MAKE_OPTS = \
 	SOC_FAMILY=$(SOC_FAMILY) \
-	SENSOR_MODEL=$(SENSOR_MODEL) \
+	SENSOR_MODEL=gc2053 \
 	KERNEL_VERSION=$(KERNEL_VERSION) \
 	INSTALL_MOD_PATH=$(TARGET_DIR) \
 	INSTALL_MOD_DIR=ingenic
@@ -16,13 +16,13 @@ LINUX_CONFIG_LOCALVERSION = \
 	$(shell awk -F "=" '/^CONFIG_LOCALVERSION=/ {print $$2}' $(BR2_LINUX_KERNEL_CUSTOM_CONFIG_FILE))
 
 ifeq ($(BR2_SOC_INGENIC_T10)$(BR2_SOC_INGENIC_T20)$(BR2_SOC_INGENIC_T30),y)
-SENSOR_CONFIG_NAME = $(SENSOR_MODEL).bin
+SENSOR_CONFIG_NAME = gc2053.bin
 else
-SENSOR_CONFIG_NAME = $(SENSOR_MODEL)-$(SOC_FAMILY).bin
+SENSOR_CONFIG_NAME = gc2053-$(SOC_FAMILY).bin
 endif
 
 ifeq ($(KERNEL_VERSION_4),y)
-FULL_KERNEL_VERSION = 4.4.94
+FULL_KERNEL_VERSION = 4.9.37
 else
 FULL_KERNEL_VERSION = 3.10.14
 endif
@@ -32,8 +32,8 @@ define INGENIC_SDK_INSTALL_TARGET_CMDS
 	touch $(TARGET_DIR)/lib/modules/$(FULL_KERNEL_VERSION)$(call qstrip,$(LINUX_CONFIG_LOCALVERSION))/modules.builtin.modinfo
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/sensor
-	$(INSTALL) -m 644 -D $(@D)/sensor-iq/$(SOC_FAMILY)/$(SENSOR_MODEL).bin $(TARGET_DIR)/etc/sensor/$(SENSOR_CONFIG_NAME)
-	echo $(SENSOR_MODEL) >$(TARGET_DIR)/etc/sensor/model
+	$(INSTALL) -m 644 -D $(@D)/sensor-iq/$(SOC_FAMILY)/gc2053.bin $(TARGET_DIR)/etc/sensor/$(SENSOR_CONFIG_NAME)
+	echo gc2053 >$(TARGET_DIR)/etc/sensor/model
 
 	$(INSTALL) -m 755 -d $(TARGET_DIR)/etc/modules.d
 
@@ -52,7 +52,7 @@ define INGENIC_SDK_INSTALL_TARGET_CMDS
 		echo "sample_pwm_hal" >> $(TARGET_DIR)/etc/modules.d/pwm; \
 	fi
 
-	echo "sensor_$(SENSOR_MODEL)_$(SOC_FAMILY) $(BR2_SENSOR_PARAMS)" > $(TARGET_DIR)/etc/modules.d/sensor
+	echo "sensor_gc2053_$(SOC_FAMILY) $(BR2_SENSOR_PARAMS)" > $(TARGET_DIR)/etc/modules.d/sensor
 endef
 
 $(eval $(kernel-module))
