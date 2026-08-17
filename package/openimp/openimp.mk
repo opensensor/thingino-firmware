2bbc586b667343f82bfa3e98625483ca5673cbc8
Matt Davis <matteius@gmail.com>
2026-08-17 18:25:32 -0400

    openisp: refresh runtime control integration
    Signed-off-by: Matt Davis <matteius@gmail.com>

--- package/openimp/openimp.mk
+++ package/openimp/openimp.mk
@@ -7,7 +7,7 @@
 OPENIMP_SITE_METHOD = git
 OPENIMP_SITE = https://github.com/opensensor/openimp
 OPENIMP_SITE_BRANCH = main
-OPENIMP_VERSION = b5316702187f33e40dda6f5d954c271107ad2543
+OPENIMP_VERSION = aa9686ffc0f89e607bc99093a231a6a3a162da69
 
 # Upstream describes OpenIMP as MIT but does not currently ship a top-level
 # license file for legal-info to collect.
