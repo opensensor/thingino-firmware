661c4fee2c19809981169f7938cdef25de14c580
Matt Davis <matteius@gmail.com>
2026-08-26 09:17:08 -0400

    vdb2: build latest open media stack with V4L2

--- package/open-tx-isp/open-tx-isp.mk
+++ package/open-tx-isp/open-tx-isp.mk
@@ -7,7 +7,7 @@
 OPEN_TX_ISP_SITE_METHOD = git
 OPEN_TX_ISP_SITE = https://github.com/opensensor/open-tx-isp
 OPEN_TX_ISP_SITE_BRANCH = main
-OPEN_TX_ISP_VERSION = 86180f8e15828cb3522980f0bc9046722eaf91f2
+OPEN_TX_ISP_VERSION = 0f442a27fc15f68d911a8204556aa40716997862
 
 # Upstream identifies the project as GPLv3 but does not currently ship a
 # top-level license file for legal-info to collect.
@@ -29,6 +29,30 @@ OPEN_TX_ISP_MODULE_MAKE_OPTS += \
 	EXTRA_CFLAGS="-I$(LINUX_DIR)/arch/mips/xburst/soc-$(SOC_FAMILY)/include \
 	-I$(LINUX_DIR)/arch/mips/xburst/core/include \
 	-I$(LINUX_DIR)/arch/mips/xburst/common/include"
+
+# Keep the public capture adapter optional: the open driver also supports the
+# legacy IMP ABI, while direct V4L2 users require both the adapter and DMA-BUF.
+ifeq ($(BR2_PACKAGE_OPEN_TX_ISP_V4L2),y)
+ifeq ($(SOC_FAMILY),t31)
+OPEN_TX_ISP_MODULE_MAKE_OPTS += CONFIG_TX_ISP_T31_V4L2=y
+else ifeq ($(SOC_FAMILY),t41)
+OPEN_TX_ISP_MODULE_MAKE_OPTS += CONFIG_TX_ISP_T41_V4L2=y
+endif
+
+ifeq ($(KERNEL_VERSION_4),y)
+OPEN_TX_ISP_DMA_CONFIG = CONFIG_THINGINO_VIDEOBUF2_DMA_CONTIG
+else
+OPEN_TX_ISP_DMA_CONFIG = CONFIG_THINGINO_DMA_SHARED_BUFFER
+endif
+
+define OPEN_TX_ISP_LINUX_CONFIG_FIXUPS
+	$(call KCONFIG_ENABLE_OPT,CONFIG_MEDIA_SUPPORT)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_MEDIA_CAMERA_SUPPORT)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_VIDEO_DEV)
+	$(call KCONFIG_ENABLE_OPT,CONFIG_VIDEO_V4L2)
+	$(call KCONFIG_ENABLE_OPT,$(OPEN_TX_ISP_DMA_CONFIG))
+endef
+endif
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
