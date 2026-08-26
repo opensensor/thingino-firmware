459a189e280d14a6bbd06fd83ec3052327a09a41
Matt Davis <matteius@gmail.com>
2026-08-26 05:30:30 -0400

    t30: add open V4L2 stack for Wyze VDB1
    Signed-off-by: Matt Davis <matteius@gmail.com>

--- package/open-tx-isp/open-tx-isp.mk
+++ package/open-tx-isp/open-tx-isp.mk
@@ -7,7 +7,7 @@
 OPEN_TX_ISP_SITE_METHOD = git
 OPEN_TX_ISP_SITE = https://github.com/opensensor/open-tx-isp
 OPEN_TX_ISP_SITE_BRANCH = main
-OPEN_TX_ISP_VERSION = 86180f8e15828cb3522980f0bc9046722eaf91f2
+OPEN_TX_ISP_VERSION = ae0c772aa7a0b07f45c17332ff0025305bb4395b
 
 # Upstream identifies the project as GPLv3 but does not currently ship a
 # top-level license file for legal-info to collect.
