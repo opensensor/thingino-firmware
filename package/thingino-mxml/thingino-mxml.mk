cf68c0c02a780442fda02639b88429f3be6897ce
Matt Davis <matteius@gmail.com>
2026-08-27 03:29:42 -0400

    package/thingino-mxml: register selected meta-package

--- package/thingino-mxml/thingino-mxml.mk
+++ package/thingino-mxml/thingino-mxml.mk
@@ -7,4 +7,7 @@
 # Actual overrides live in mxml-override.mk.
 THINGINO_MXML_DEPENDENCIES = mxml
 
-$(eval $(virtual-package))
+# This is a selected meta-package that applies the mxml override above.  It is
+# not a Buildroot virtual package: those use BR2_PACKAGE_HAS_* plus a provider,
+# which makes the dependency checker ignore BR2_PACKAGE_THINGINO_MXML.
+$(eval $(generic-package))
