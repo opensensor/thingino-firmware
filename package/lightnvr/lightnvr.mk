8da939261e6b00958fc8fa84f011893ac66b48c1
Matt Davis <matteius@gmail.com>
2026-08-29 02:35:16 -0400

    lightnvr: add required zlib dependency
    Signed-off-by: Matt Davis <matteius@gmail.com>

--- package/lightnvr/lightnvr.mk
+++ package/lightnvr/lightnvr.mk
@@ -9,7 +9,7 @@ LIGHTNVR_LICENSE_FILES = COPYING
 LIGHTNVR_INSTALL_STAGING = YES
 
 # Dependencies
-LIGHTNVR_DEPENDENCIES = thingino-ffmpeg thingino-libcurl sqlite host-nodejs cjson libuv llhttp go2rtc
+LIGHTNVR_DEPENDENCIES = thingino-ffmpeg thingino-libcurl sqlite host-nodejs cjson libuv llhttp go2rtc zlib
 HOST_LIGHTNVR_DEPENDENCIES = host-nodejs
 
 ifeq ($(BR2_PACKAGE_MBEDTLS),y)
