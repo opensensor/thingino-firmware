3d76bbffa4b08b2ca34687f165b902448c966c15
Matt Davis <matteius@gmail.com>
2026-09-07 16:18:03 -0400

    package/openimp: update to a06a0fb
    Update openimp from dfccfd4 to a06a0fb
    
    Hash change: dfccfd48958f3257607a7a8786808f9d60ba32af -> a06a0fb29af31e9d248f556d8729a8cde67e1926
    
    Changelog:
    
      a06a0fb: tuning: prefer calibrated T41 AE and preserve WB instead of scene presets
    
    Signed-off-by: Matt Davis <matteius@gmail.com>

--- package/openimp/openimp.mk
+++ package/openimp/openimp.mk
@@ -7,7 +7,7 @@
 OPENIMP_SITE_METHOD = git
 OPENIMP_SITE = https://github.com/opensensor/openimp
 OPENIMP_SITE_BRANCH = dev/t41-stack-20260907
-OPENIMP_VERSION = dfccfd48958f3257607a7a8786808f9d60ba32af
+OPENIMP_VERSION = a06a0fb29af31e9d248f556d8729a8cde67e1926
 
 # Upstream describes OpenIMP as MIT but does not currently ship a top-level
 # license file for legal-info to collect.
